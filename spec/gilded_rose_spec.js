7f1b04f3ea2e1d3a3df77faee6d7d1819ec060bb
David Herbst Ayala <mail@david-it.dev>
2021-03-17 23:22:19 +0100

    wrote descriptions for test cases in spec file

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,49 @@
-describe("Gilded Rose", function() {
+//sell_in value denotes number of days left to sell the item
+//quality denotes valuability
+//'end of day' both values get lowered
 
-  it("should do something", function() {
+describe("Gilded Rose", function() {
+  it("should do return result", () => {
     update_quality();
   });
+
+  it("should degrade quality twice as fast, if sell_in is less then 0", () => {
+
+  });
+
+  it("should never let an item quality be negative", () => {
+
+  });
+
+  it("should never let an item quality be higher then 50. except 'Sulfuras'", () => {
+
+  });
+
+  it("should let 'Sulfuras' quality reach 80", () => {
+
+  })
+
+  it("should increase 'Aged Brie's quality the older it gets", () => {
+
+  });
+
+  it("should never decrease quality of 'Sulfuras'", () => {
+
+  });
+
+  it("doenst have to sell 'Sulfuras'", () => {
+
+  });
+
+  it("should increase quality of 'Backstage passes' if sell_in decreases. When sell_in < 10 increase: 2, when sell_in < 5 increase: 3", () => {
+
+  });
+
+  it("'Backstage passes' quality turns 0 after the concert", () => {
+
+  });
+
+
+
 
 });
